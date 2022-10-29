d939e7fdf87ffcebf233a2b65e0f188c1df5f510
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-10-29 17:55:34 +0200

    Adding Hello World example

--- hello-world/build-hello.sh
+++ hello-world/build-hello.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Original script from: https://github.com/shaunsmith/tiny-java-containers/tree/main/helloworld
+
+java -version
 javac Hello.java
-native-image --static --libc=musl -o hello.static Hello
+#native-image --static --libc=musl -o hello.static Hello
+native-image --native-image-info --verbose --static -o hello.static Hello
 docker build . -f Dockerfile.static -t hello:static
 rm -f hello.upx
 upx --lzma --best -o hello.upx hello.static
@@ -12,5 +16,9 @@ ls -lh hello.static hello.upx
 
 echo "Generated Docker Container Images"
 docker images hello
+
+echo "Run images as final E2E tests"
+time docker run hello:static
+time docker run hello:upx
 
 
---
+++ hello-world/prepare-environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install -y wget gcc libz-dev upx musl-tools
+sudo rm -rf /var/lib/apt/lists/*
+export VERSION=22.3.0
+wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-$VERSION/graalvm-ce-java19-linux-amd64-$VERSION.tar.gz
+sudo tar -xvzf graalvm-ce-java19-linux-amd64-$VERSION.tar.gz
+sudo rm graalvm-ce-java19-linux-amd64-$VERSION.tar.gz
+sudo $(pwd)/graalvm-ce-java19-$VERSION/bin/gu install native-image
+export PATH=$(pwd)/graalvm-ce-java19-$VERSION/bin:$PATH
