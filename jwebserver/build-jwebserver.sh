8570f8d52d5d89373c1a1624100d6beb12003c12
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-10-29 18:17:02 +0200

    Improving pipeline

--- jwebserver/build-jwebserver.sh
+++ jwebserver/build-jwebserver.sh
@@ -2,7 +2,9 @@
 
 # Original script from: https://github.com/shaunsmith/tiny-java-containers/tree/main/jwebserver
 
-native-image --static --libc=musl -m jdk.httpserver -o jwebserver.static
+#native-image --static --libc=musl -m jdk.httpserver -o jwebserver.static
+native-image --native-image-info --verbose --static -m jdk.httpserver -o jwebserver.static
+
 docker build . -f Dockerfile.static -t jwebserver:static
 rm -f jwebserver.upx
 upx --lzma --best -o jwebserver.upx jwebserver.static 
@@ -13,5 +15,9 @@ ls -lh jwebserver.static jwebserver.upx
 
 echo "Generated Docker Container Images"
 docker images jwebserver
+
+echo "Run images as final E2E tests"
+time docker run jwebserver:static
+time docker run jwebserver:upx
 
 
