701f81b59c4b5a33a53134cdc46198002c4cc005
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-02 17:18:33 +0100

    Initial steps to refactor the examples

--- tiny-java-containers/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x
-
-rm -rf  x86_64-linux-musl-native  zlib-*
-./helloworld/clean.sh
-./jwebserver/clean.sh
--- tiny-java-containers/jwebserver/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf jwebserver-jlink/
-rm jwebserver.dynamic  jwebserver.mostly  jwebserver.static  jwebserver.static-upx
-docker images jwebserver -q | grep -v TAG | awk '{print($1)}' | xargs docker rmi
