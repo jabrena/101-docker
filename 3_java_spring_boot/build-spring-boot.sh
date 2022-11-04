cf796505b60b384cc1cd3122266fa46bb3bbd87d
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-04 13:10:58 +0100

    Adding support for buildpacks

--- 3_java_spring_boot/build-spring-boot.sh
+++ 3_java_spring_boot/build-spring-boot.sh
@@ -7,7 +7,7 @@ PATH=${TOOLCHAIN_DIR}/bin:${PATH}
 set -x
 
 echo "Generated Executables with buildpacks"
-mvn -Pnative spring-boot:build-image
-mvn spring-boot:build-image
+mvn -Pnative spring-boot:build-image -Dspring-boot.build-image.imageName=benchmark-jibber:buildpacks-native.0.0.1-SNAPSHOT
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=benchmark-jibber:buildpacks-jvm.0.0.1-SNAPSHOT
 
 docker images benchmark-jibber
