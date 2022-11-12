6bf0f3fc228bac14a2149ecb84476e80325f5480
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-12 18:44:22 +0100

    Testing builder from Dashaun

--- poliglot_docker_builds/3_1_java_spring_boot_m1/build-spring-boot.sh
+++ poliglot_docker_builds/3_1_java_spring_boot_m1/build-spring-boot.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-TOOLCHAIN_DIR=`pwd`/../../x86_64-linux-musl-native
-CC=${TOOLCHAIN_DIR}/bin/gcc
-PATH=${TOOLCHAIN_DIR}/bin:${PATH}
-
-set -x
-
 echo "Generated Executables with buildpacks"
 mvn -Pnative spring-boot:build-image
