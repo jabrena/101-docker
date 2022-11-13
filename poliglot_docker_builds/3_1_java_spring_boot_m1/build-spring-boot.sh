d9cb4f12d73f0d16d8947264ebb37be7edeab879
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-13 15:55:19 +0100

    Adding UPX to native compilation

--- poliglot_docker_builds/3_1_java_spring_boot_m1/build-spring-boot.sh
+++ poliglot_docker_builds/3_1_java_spring_boot_m1/build-spring-boot.sh
@@ -2,3 +2,6 @@
 
 echo "Generated Executables with buildpacks"
 mvn -Pnative spring-boot:build-image
+
+echo "Generated Docker Container Images Summary"
+docker images dashaun --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
