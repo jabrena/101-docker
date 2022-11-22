ff33150ecd4ba7113294be7b0f1d830dd575d504
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-22 21:22:23 +0100

    Adding support for FASTAPI

--- poliglot_docker_builds/5_python/build-python.sh
+++ poliglot_docker_builds/5_python/build-python.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-docker build . -f Dockerfile.http_server -t py:http_server
+cd http-server
+docker build . -f Dockerfile -t py:http_server.0.0.1-SNAPSHOT
+cd ..
 
-docker build . -f Dockerfile.django -t py:django.0.0.1-SNAPSHOT
+cd django
+docker build . -f Dockerfile -t py:django.0.0.1-SNAPSHOT
 #docker run -d -p 8000:8000 py:django.0.0.1-SNAPSHOT
 #curl http://localhost:8000/
+cd ..
 
+cd fastapi
+docker build . -f Dockerfile -t py:fastapi.0.0.1-SNAPSHOT
+#docker run -d -p 8000:8000 py:django.0.0.1-SNAPSHOT
+#curl http://localhost:8000/
+cd ..
 
 echo "Generated Docker Container Images Summary"
 docker images py --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
