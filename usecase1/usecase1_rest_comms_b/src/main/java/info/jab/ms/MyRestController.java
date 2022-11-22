c0c345927b792ba994e9e3a9a9c8ca6e2539616b
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-22 16:58:10 +0100

    Adding initial support for Jmeter;
    In local, it is not possible to test the combination: Docker-compose + native image

--- usecase1/usecase1_rest_comms_b/src/main/java/info/jab/ms/MyRestController.java
+++ usecase1/usecase1_rest_comms_b/src/main/java/info/jab/ms/MyRestController.java
@@ -25,11 +25,6 @@ public class MyRestController {
 
     @GetMapping("/")
     public ResponseEntity<String> getData() {
-        try {
-            Thread.sleep(3_000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         return ResponseEntity.ok().body("Hello World");
     }
 
