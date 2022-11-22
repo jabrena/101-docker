65127c9bad2ea65af4d187ec3761d238ad860980
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-22 18:02:08 +0100

    Feature/jmeter1 (#61)
    * Adding initial support for Jmeter; failing native tests
    
    * Compiling native images for Java 17
    
    * Minor change
    
    * Adding reports

--- usecase1/usecase1_build.sh
+++ usecase1/usecase1_build.sh
@@ -16,27 +16,3 @@ echo "Generated Docker Container Images Summary"
 docker images ghcr.io/jabrena/use-case1-a --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
 sleep 1
 docker images ghcr.io/jabrena/use-case1-b --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
-
-echo "Running docker compose jvm"
-docker-compose -f docker-compose-jvm.yml up -d
-
-sleep 25
-curl http://localhost:8081/
-curl http://localhost:8080/
-docker-compose -f docker-compose-jvm.yml stop
-
-echo "Running docker compose native"
-docker-compose -f docker-compose-native.yml up -d
-
-sleep 5
-curl http://localhost:8081/
-curl http://localhost:8080/
-docker-compose -f docker-compose-native.yml logs -f usecase1a &
-docker-compose -f docker-compose-native.yml stop
-
-#Logs
-#docker-compose -f docker-compose-jvm.yml logs -f usecase1a
-#docker-compose -f docker-compose-native.yml logs -f usecase1a
-#docker-compose logs -f usecase1b
-
-#docker stats
