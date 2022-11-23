6b2ddaae89fbe5c437bf7daa7288ad03a7580fcb
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-23 17:05:16 +0100

    Initial Api gateway development

--- usecase1/usecase1_build.sh
+++ usecase1/usecase1_build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "Building image for usecase1_rest_api_gateway"
+cd usecase1_api_gateway
+mvn spring-boot:build-image
+mvn -Pnative spring-boot:build-image
+cd ..
+
 echo "Building image for usecase1_rest_comms_a"
 cd usecase1_rest_comms_a
 mvn spring-boot:build-image
@@ -13,6 +19,8 @@ mvn -Pnative spring-boot:build-image
 cd ..
 
 echo "Generated Docker Container Images Summary"
-docker images ghcr.io/jabrena/use-case1-a --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
+docker images ghcr.io/jabrena/usecase1-api-gateway --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
 sleep 1
-docker images ghcr.io/jabrena/use-case1-b --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
+docker images ghcr.io/jabrena/usecase1-a --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
+sleep 1
+docker images ghcr.io/jabrena/usecase1-b --format '{{.Size}}\t{{.Repository}}\t{{.Tag}}\t{{.ID}}' | sed 's/ //' | sort -h -r | column -t
