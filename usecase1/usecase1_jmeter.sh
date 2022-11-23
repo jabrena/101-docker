9a2bb2be577de81ad3412198371eb6165da681a0
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-23 15:22:15 +0100

    Adding jmeter support for Github actions

--- usecase1/usecase1_jmeter.sh
+++ usecase1/usecase1_jmeter.sh
@@ -2,6 +2,14 @@
 
 #https://downloads.apache.org//jmeter/binaries/
 
+echo "Download jmeter"
+rm -rf apache-jmeter-5.5
+wget https://downloads.apache.org/jmeter/binaries/apache-jmeter-5.5.zip
+unzip apache-jmeter-5.5.zip
+cd apache-jmeter-5.5/bin
+./jmeter -v
+cd ../../
+
 echo "Running docker compose jvm"
 docker-compose -f docker-compose-jvm.yml up -d
 sleep 25
@@ -9,31 +17,31 @@ sleep 25
 #Generate load for JVM
 rm -rf ./result-jvm.csv
 rm -rf html-jvm
-jmeter -n -t jvm-test1.jmx \
+./apache-jmeter-5.5/bin/jmeter -n -t jvm-test1.jmx \
        -l ./result-jvm.csv
 
 #Generate report
 rm -rf ./html/**
 mkdir html-jvm
-jmeter -g ./result-jvm.csv  \
+./apache-jmeter-5.5/bin/jmeter -g ./result-jvm.csv  \
   -o ./html-jvm
 
 docker-compose -f docker-compose-jvm.yml stop
 
 echo "Running docker compose native"
-docker-compose -f docker-compose-hybrid.yml up -d
+docker-compose -f docker-compose-native.yml up -d
 sleep 25
 
 #Generate load for JVM
 rm -rf ./result-native.csv
 rm -rf html-native
-jmeter -n -t jvm-test1.jmx \
+./apache-jmeter-5.5/bin/jmeter -n -t jvm-test1.jmx \
        -l ./result-native.csv
 
 #Generate report
 rm -rf ./html-native/**
 mkdir html-native
-jmeter -g ./result-native.csv  \
+./apache-jmeter-5.5/bin/jmeter -g ./result-native.csv  \
   -o ./html-native
 
-docker-compose -f docker-compose-hybrid.yml stop
+docker-compose -f docker-compose-native.yml stop
