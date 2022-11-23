fc2fabe9d7f57cb5cfdb831f6ab8fadf90bf7f9e
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-23 14:47:46 +0100

    native compilation on OSX doesn't support --static

--- usecase1/usecase1_run.sh
+++ usecase1/usecase1_run.sh
@@ -4,20 +4,16 @@ echo "Running docker compose jvm"
 docker-compose -f docker-compose-jvm.yml up -d
 
 sleep 25
-curl http://localhost:8081/
-echo ""
-curl http://localhost:8080/
-echo ""
+curl http://localhost:8081/ -w '\n'
+curl http://localhost:8080/ -w '\n'
 docker-compose -f docker-compose-jvm.yml stop
 
 echo "Running docker compose native"
 docker-compose -f docker-compose-native.yml up -d
 
 sleep 25
-curl http://localhost:8081/
-echo ""
-curl http://localhost:8080/
-echo ""
+curl http://localhost:8081/ -w '\n'
+curl http://localhost:8080/ -w '\n'
 #docker-compose -f docker-compose-native.yml logs -f usecase1a &
 docker-compose -f docker-compose-native.yml stop
 
