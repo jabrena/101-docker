1eee917c71224402aa46d3329948637bda7516de
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-28 16:07:52 +0100

    Minor refactoring

--- usecase2/usecase2_path1/build-spring-boot.sh
+++ usecase2/usecase2_path1/build-spring-boot.sh
@@ -7,21 +7,12 @@ PATH=${TOOLCHAIN_DIR}/bin:${PATH}
 set -x
 
 echo "Generated Executables with buildpacks"
-mvn -Pnative spring-boot:build-image
 mvn spring-boot:build-image
 
-mvn --no-transfer-progress native:compile -Pnative package
-
-echo "Generated Executables"
-upx --lzma --best ./target/benchmark-jibber -o ./target/benchmark-jibber-upx
-
-ldd ./target/benchmark-jibber
-ldd ./target/benchmark-jibber-upx
-
-docker build . -f Dockerfiles/Dockerfile.native --build-arg APP_FILE=benchmark-jibber-upx -t jibber-benchmark:native.0.0.1-SNAPSHOT
-docker build . -f Dockerfiles/Dockerfile.native-static-upx --build-arg APP_FILE=benchmark-jibber-upx -t jibber-benchmark:native-upx.0.0.1-SNAPSHOT
-
-echo "Run images as final E2E tests"
-docker run --rm --name native -d -p 8080:8080 jibber-benchmark:native-upx.0.0.1-SNAPSHOT
-sleep 5
+echo "Running docker compose jvm"
+docker-compose -f docker-compose.yml up -d
+sleep 60
+curl -X GET http://localhost:8080/jibber
 curl -X POST http://localhost:8080/actuator/shutdown
+
+docker-compose -f docker-compose.yml down
