f4c769fb0619eb05e83f5c97f71f8d013ea1fadd
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-29 11:45:20 +0100

    Minor change

--- usecases/demo/build-spring-boot.sh
+++ usecases/demo/build-spring-boot.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+#Not used until OSX has musl support
 TOOLCHAIN_DIR=`pwd`/../../x86_64-linux-musl-native
 CC=${TOOLCHAIN_DIR}/bin/gcc
 PATH=${TOOLCHAIN_DIR}/bin:${PATH}
 
 set -x
 
-docker login -u="$1" -p="$2"
+docker login https://index.docker.io/v1 -u="$1" -p="$2"
 
 mvn spring-boot:build-image \
     --batch-mode --no-transfer-progress
 
-BUILD_VERSION=$(mvn help:evaluate -Dexpression=docker.image -q -DforceStdout)
-echo $BUILD_VERSION
+IMAGE_NAME=$(mvn help:evaluate -Dexpression=docker.image.name -q -DforceStdout)
+echo $IMAGE_NAME
 
-docker push $BUILD_VERSION
+docker push $IMAGE_NAME
