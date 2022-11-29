c614604f44370fd323680e9d20e2f4b8a80deec3
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-11-29 11:12:00 +0100

    Minor change

--- usecases/demo/build-spring-boot.sh
+++ usecases/demo/build-spring-boot.sh
@@ -7,4 +7,14 @@ PATH=${TOOLCHAIN_DIR}/bin:${PATH}
 set -x
 
 echo "Generated Executables with buildpacks"
-mvn spring-boot:build-image
+#mvn spring-boot:build-image
+
+#docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+
+docker login -u="$1" -p="$2"
+
+mvn spring-boot:build-image \
+            --batch-mode --no-transfer-progress \
+            -Dspring-boot.build-image.imageName="jabrena/demo-ms:0.1.0"
+
+#docker push docker.io/jabrena/demo-ms:0.1.0
